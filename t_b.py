e920bf57007f6b4101f0bbcfd9d2a75b04c3754f
ArsenyIT <st.2.udalov@gmail.com>
2024-11-12 10:29:32 +0300

    12.11.2024

--- t_b.py
+++ t_b.py
@@ -4,8 +4,8 @@ API_TOKEN = '7596980839:AAEAyGgj5K2s0A7f4IJYUw52WnmiD3nnB6w'
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    if message.text.lower() == "привет!":
-        bot.reply_to(message, "И тебе всего хорошего!!!")
+def send_greeting(message):
+    bot.reply_to(message, "Привет! И тебе всего хорошего!!!")
 
-bot.polling()
+if __name__ == '__main__':
+    bot.polling()
